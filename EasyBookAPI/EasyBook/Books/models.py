71302d26cec0806a38d8a354721392cb60094879
Narciso <narciso.cordeiro18@gmail.com>
2024-05-24 12:32:30 +0100

    Completados modelos y migraciones

--- EasyBookAPI/EasyBook/Books/models.py
+++ EasyBookAPI/EasyBook/Books/models.py
@@ -30,3 +30,19 @@ class BooksReadByUser(models.Model):
 
     def __str__(self):
         return f'{self.book.title} - {self.user.username}'
+
+class BooksAbandonedByUser(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    note = models.CharField(max_length=2500, null=True)
+
+    def __str__(self):
+        return f'{self.book.title} - {self.user.username}'
+
+class BooksToBeReadByUser(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    note = models.CharField(max_length=2500, null=True)
+
+    def __str__(self):
+        return f'{self.book.title} - {self.user.username}'
