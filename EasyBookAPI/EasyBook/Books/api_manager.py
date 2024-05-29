c24a2ba5b2702b3a6f673f98481c8b4087d92b8c
Narciso <narciso.cordeiro18@gmail.com>
2024-05-29 22:43:02 +0100

    Pequena correcion

--- EasyBookAPI/EasyBook/Books/api_manager.py
+++ EasyBookAPI/EasyBook/Books/api_manager.py
@@ -1,14 +1,17 @@
 import requests
 import json
 
+class api_manager:
+    print('')
+
 headers = {
     'User-Agent': 'OrbisTertius/1.0 (narciso.cordeiro18@gmail.com)'
 }
-query = ''
+query = 'outer dark'
 url = f'https://openlibrary.org/search.json?q={query}'
 
 response = requests.get(url, headers=headers)
-response_json = response.json()
-print(response_json['docs'][0])
-#for key in response_json:
-#    print(response_json[key])
+json_data = response.json()
+
+
+print(json_data)
