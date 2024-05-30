85c57bd29a3141c67b216633350b17e594e68db0
Narciso <narciso.cordeiro18@gmail.com>
2024-05-30 14:29:47 +0100

    Cambios menores

--- EasyBookAPI/EasyBook/Books/urls.py
+++ EasyBookAPI/EasyBook/Books/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, include
-from . import views
+from . import views, endpoints
 from .views import CreateUserView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('create-user/', CreateUserView.as_view(), name='create-user'),
     path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh')
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('test/', endpoints.test2),
+    path('test/<int:testId>', endpoints.test),
 ]
